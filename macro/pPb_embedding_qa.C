370758b446e7791e15d59d927201b2debfafd6bc
Grigory Nigmatkulov <nigmatkulov@gmail.com>
2024-05-06 13:28:06 -0500

    Update with fakes

--- macro/pPb_embedding_qa.C
+++ macro/pPb_embedding_qa.C
@@ -11,11 +11,31 @@
 #include "TMultiGraph.h"
 #include "TAxis.h"
 #include "TLegend.h"
-#include "TEfficiency.h"
 #include "TLine.h"
+#include "TDatime.h"
+#include "TSystem.h"
+#include "TLatex.h"
+#include "TSystemDirectory.h"
+#include <sys/stat.h>
 
 // C++ headers
 #include <iostream>
+
+//________________
+bool directoryExists(const char* directoryPath) {
+    TSystemFile file(directoryPath, "");
+    return file.IsDirectory();
+}
+
+//________________
+void createDirectory(const char* directoryPath) {
+    // Create the directory with read, write, and execute permissions for owner, group, and others
+    if (mkdir(directoryPath, S_IRWXU | S_IRWXG | S_IRWXO) == 0) {
+        std::cout << "Directory created successfully." << std::endl;
+    } else {
+        std::cerr << "Failed to create directory." << std::endl;
+    }
+}
 
 //________________
 void setStyle() {
@@ -169,7 +189,7 @@ void plotEfficiency(TFile *inFile, TString date) {
     TCanvas *cEfficiency = new TCanvas("cEfficiency","cEfficiency", 1200, 800);
     setPadStyle();
     hEfficiency->Draw("colz");
-    gPad->SetLogz(1);
+    //gPad->SetLogz(1);
 
     cEfficiency->SaveAs(Form("%s/pPb8160_pt_vs_eta_efficiency.pdf", date.Data()));
 
@@ -212,7 +232,9 @@ void plotEfficiency(TFile *inFile, TString date) {
         }
         t.DrawLatexNDC(0.2, 0.9, Form("%4.1f < p_{T} (GeV/c) < %4.1f", 
                        fPtRange[0] + (i-1) * ptStep, fPtRange[0] + i * ptStep) );
+    } // for (Int_t i{1}; i<=fPtBins; i++)
 
+    for (Int_t i{1}; i<=fEtaBins; i++) {
         // Project on pT
         hPtEfficiency[i] = (TH1D*)hEfficiency->ProjectionY(Form("hPtEfficiency_%d", i), i, i);
         hPtEfficiency[i]->SetNameTitle(Form("hPtEfficiency_%d", i), ";p_{T} (GeV/c);Efficiency");
@@ -224,7 +246,7 @@ void plotEfficiency(TFile *inFile, TString date) {
         hPtEfficiency[i]->GetYaxis()->SetRangeUser(0.9, 1.02);
         t.DrawLatexNDC(0.2, 0.9, Form("%2.1f < #eta < %2.1f", 
                        fEtaRange[0] + (i-1) * etaStep, fEtaRange[0] + i * etaStep) );
-    } // for (Int_t i{1}; i<=fPtBins; i++)
+    } // for (Int_t i{1}; i<=fEtaBins; i++)
 
     cEtaEfficiency->SaveAs( Form("%s/pPb8160_eta_efficiency_projections.pdf", date.Data()) );
     cPtEfficiency->SaveAs( Form("%s/pPb8160_pt_efficiency_projections.pdf", date.Data()) );
@@ -285,7 +307,6 @@ void plotEtaDijetCorrelation(TFile *inFile, TString date) {
 
     cDijetEtaRefVsReco->SaveAs(Form("%s/pPb8160_ref_vs_reco_responce.pdf", date.Data()));
 }
-
 
 //________________
 void plotDijetDistributions(TFile *inFile, TString date) {
@@ -467,28 +488,120 @@ void compareInclusiveJetPtSpectra(TFile *inFile, TString date) {
 
 //________________
 void plotRecoAndFakes(TFile *inFile, TString date) {
+    // Retrieve reco
     TH2D* hRecoInclusiveAllJetPtVsEta = (TH2D*)inFile->Get("hRecoInclusiveAllJetPtVsEta");
+    // Retrieve reco that matched gen
     TH2D* hRecoInclusiveMatchedJetPtVsEta = (TH2D*)inFile->Get("hRecoInclusiveMatchedJetPtVsEta");
-    TH2D* hNumberOfFakes = new TH2D("hNumberOfFakes","Number of fakes (reco - recoMatched);#eta;p_{T} (GeV/c)",
-                                    hRecoInclusiveAllJetPtVsEta->GetNbinsX(), hRecoInclusiveAllJetPtVsEta->GetXaxis()->GetXbins()->GetArray(),
-                                    hRecoInclusiveAllJetPtVsEta->GetNbinsY(), hRecoInclusiveAllJetPtVsEta->GetYaxis()->GetXbins()->GetArray());
-    hNumberOfFakes->Sumw2();
-    hNumberOfFakes->Add(hRecoInclusiveAllJetPtVsEta, hRecoInclusiveMatchedJetPtVsEta, 1., -1.);
+    // Create a histogram to calculate fakes
+    TH2D* hNumberOfFakes = (TH2D*)hRecoInclusiveAllJetPtVsEta->Clone("hNumberOfFakes");
+    hNumberOfFakes->SetTitle("Number of fakes (reco - recoMatched);#eta;p_{T} (GeV/c)");
+    hNumberOfFakes->Add(hRecoInclusiveMatchedJetPtVsEta, -1.);
 
+    // z axis scaling
+    Double_t zAxisRange[2] {0., hRecoInclusiveAllJetPtVsEta->GetMaximum()};
+    TLatex t;
+    t.SetTextFont(42);
+    t.SetTextSize(0.06);
+
+    // Create canvas to plot 2D distributions
     TCanvas *cFakes2D = new TCanvas("cFakes2D","cFakes2D", 1300, 400);
     cFakes2D->Divide(3, 1);
 
+    // Plot 2D reco
     cFakes2D->cd(1);
     setPadStyle();
     hRecoInclusiveAllJetPtVsEta->Draw("colz");
+    t.DrawLatexNDC(0.46, 0.96, Form("Reco") );
 
+    // Plot 2D reco that matched gen
     cFakes2D->cd(2);
     setPadStyle();
     hRecoInclusiveMatchedJetPtVsEta->Draw("colz");
+    hRecoInclusiveMatchedJetPtVsEta->GetZaxis()->SetRangeUser(zAxisRange[0], zAxisRange[1]);
+    t.DrawLatexNDC(0.4, 0.96, Form("Reco matched") );
 
+    // Plot 2D fakes
     cFakes2D->cd(3);
     setPadStyle();
-    cFakes2D->Draw("colz");
+    hNumberOfFakes->Draw("colz");
+    hNumberOfFakes->GetZaxis()->SetRangeUser(zAxisRange[0], zAxisRange[1]);
+    t.DrawLatexNDC(0.46, 0.96, Form("Fakes") );
+
+    //
+    // Plot projections of fakes as a function of eta and pT
+    //
+
+    TH2D *hRecoMatchedJetFrac = (TH2D*)hRecoInclusiveMatchedJetPtVsEta->Clone("hRecoMatchedJetFrac");
+    TH2D *hRecoFakeJetFrac = (TH2D*)hNumberOfFakes->Clone("hRecoFakeJetFrac");
+    hRecoMatchedJetFrac->Divide(hRecoMatchedJetFrac, hRecoInclusiveAllJetPtVsEta, 1., 1., "b");
+    hRecoFakeJetFrac->Divide(hRecoFakeJetFrac, hRecoInclusiveAllJetPtVsEta, 1., 1., "b");
+
+    Int_t fPtBins = hNumberOfFakes->GetNbinsY();
+    Double_t fPtRange[2] {hNumberOfFakes->GetYaxis()->GetBinLowEdge(1), 
+                          hNumberOfFakes->GetYaxis()->GetBinUpEdge(fPtBins)};
+    Int_t fEtaBins = hNumberOfFakes->GetNbinsX();
+    Double_t fEtaRange[2] {hNumberOfFakes->GetXaxis()->GetBinLowEdge(1), 
+                           hNumberOfFakes->GetXaxis()->GetBinUpEdge(fEtaBins)};
+    Double_t ptStep = (fPtRange[1]-fPtRange[0]) / fPtBins;
+    Double_t etaStep = (fEtaRange[1]-fEtaRange[0]) / fEtaBins;
+
+    TH1D *hEtaFakes[fPtBins];
+    TH1D *hPtFakes[fEtaBins];
+    TH1D *hEtaMatched[fPtBins];
+    TH1D *hPtMatched[fEtaBins];
+
+    TCanvas *cEtaFakes = new TCanvas("cEtaFakes", "cEtaFakes", 1600, 800);
+    cEtaFakes->Divide(5, ( (fPtBins % 5) == 0 ) ? (fPtBins / 5) : (fPtBins / 5 + 1) );
+
+    TCanvas *cPtFakes = new TCanvas("cPtFakes", "cPtFakes", 1600, 800);
+    cPtFakes->Divide(10, 5);
+
+    // Make projections bin-by-bin on eta
+    for (Int_t i{1}; i<=fPtBins; i++) {
+        // Project on eta
+        hEtaFakes[i] = (TH1D*)hRecoFakeJetFrac->ProjectionX(Form("hEtaFakes_%d", i), i, i);
+        hEtaFakes[i]->SetNameTitle(Form("hEtaFakes_%d", i), ";#eta;Efficiency");
+        set1DStyle(hEtaFakes[i], 2);
+        hEtaFakes[i]->SetMarkerSize(0.7);
+
+        hEtaMatched[i] = (TH1D*)hRecoMatchedJetFrac->ProjectionX(Form("hEtaMatched_%d", i), i, i);
+        hEtaMatched[i]->SetNameTitle(Form("hEtaMatched_%d", i), ";#eta;Efficiency");
+        set1DStyle(hEtaMatched[i], 1);
+        hEtaMatched[i]->SetMarkerSize(0.7);
+
+        cEtaFakes->cd(i);
+        setPadStyle();
+        hEtaFakes[i]->Draw();
+        hEtaMatched[i]->Draw("same");
+        hEtaFakes[i]->GetYaxis()->SetRangeUser(0., 1.01);
+        t.DrawLatexNDC(0.35, 0.95, Form("%4.1f < p_{T} (GeV/c) < %4.1f", 
+                       fPtRange[0] + (i-1) * ptStep, fPtRange[0] + i * ptStep) );
+    } // for (Int_t i{1}; i<=fPtBins; i++)
+
+    // Make projections bin-by-bin on pT
+    for (Int_t i{1}; i<=fEtaBins; i++) {
+        // Project on pT
+        hPtFakes[i] = (TH1D*)hRecoFakeJetFrac->ProjectionY(Form("hPtFakes_%d", i), i, i);
+        hPtFakes[i]->SetNameTitle(Form("hPtFakes_%d", i), ";p_{T} (GeV/c);Efficiency");
+        set1DStyle(hPtFakes[i], 2);
+        hPtFakes[i]->SetMarkerSize(0.7);
+
+        hPtMatched[i] = (TH1D*)hRecoMatchedJetFrac->ProjectionY(Form("hPtMatched_%d", i), i, i);
+        hPtMatched[i]->SetNameTitle(Form("hPtMatched_%d", i), ";p_{T} (GeV/c);Efficiency");
+        set1DStyle(hPtMatched[i], 1);
+        hPtMatched[i]->SetMarkerSize(0.7);
+
+        cPtFakes->cd(i);
+        setPadStyle();
+        hPtFakes[i]->Draw();
+        hPtMatched[i]->Draw("same");
+        hPtFakes[i]->GetYaxis()->SetRangeUser(0., 1.01);
+        t.DrawLatexNDC(0.35, 0.95, Form("%2.1f < #eta < %2.1f", 
+                       fEtaRange[0] + (i-1) * etaStep, fEtaRange[0] + i * etaStep) );
+    }
+
+    cEtaFakes->SaveAs( Form("%s/pPb8160_eta_fakes_projections.pdf", date.Data()) );
+    cPtFakes->SaveAs( Form("%s/pPb8160_pt_fakes_projections.pdf", date.Data()) );
 }
 
 //________________
@@ -498,7 +611,17 @@ void pPb_embedding_qa(const Char_t *inFileName = "../build/oEmbedding_pPb8160_Pb
     gStyle->SetOptTitle(0);
     gStyle->SetPalette(kBird);
 
-    TString date {"20240503"};
+    TDatime dt;
+    TString date { Form( "%d",dt.GetDate() ) };
+
+    if ( directoryExists( date.Data() ) ) {
+        //std::cout << "Directory exists." << std::endl;
+    } 
+    else {
+        createDirectory( date.Data() );
+    }
+
+
     TFile *inFile = TFile::Open(inFileName);
     if ( !inFile ) {
         std::cout << "[ERROR] Input file does not exist\n";
@@ -516,13 +639,10 @@ void pPb_embedding_qa(const Char_t *inFileName = "../build/oEmbedding_pPb8160_Pb
     //plotEfficiency(inFile, date);
 
     // Plot dijet distributions
-    //plotDijetDistributions(inFile, date);
-
-    // Plot various dijet distributions
-    //plotDijetDistributions(inFile, date);
+    plotDijetDistributions(inFile, date);
 
     // Plot reco, reco with matching and calculate fakes
-    plotRecoAndFakes(inFile, date);
+    // plotRecoAndFakes(inFile, date);
 
     // Plot correlation between ref and reco dijet eta
     //plotEtaDijetCorrelation(inFile, date);
