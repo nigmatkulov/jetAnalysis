e012be6f92b2b3c3b0f434645ab780d2e2a1328c
Grigory Nigmatkulov <nigmatkulov@gmail.com>
2024-06-05 03:29:26 +0200

    Update processing script for pPb data

---
+++ processing/split_dijet_input.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <input_file> <N> <sample_name> <direction> <pd_number>"
+    exit 1
+fi
+
+# Date of submission
+formatted_date=$(date +"%Y%m%d")
+
+# Input file name
+input_file="$1"
+# Number of entries for each sublist
+N="$2"
+# Sample name
+sample_name="$3"
+# Direction
+direction="$4"
+# PD number
+pd_number="$5"
+
+# Prefix
+sample_prefix="inputfile"
+if [ "$sample_name" == "DATA_MB" ]; then
+    ${sample_prefix}=MB_PD${pd_number}_${direction}
+elif [ "$sample_name" == "DATA_HM185" ]; then
+    ${sample_prefix}=HM185_PD${pd_number}_${direction}
+else
+    ${sample_prefix}=HM185_${direction}
+fi
+
+# Check if the input file exists
+if [ ! -f "$input_file" ]; then
+    echo "Input file '$input_file' not found."
+    exit 1
+fi
+
+# Count the total number of lines in the input file
+total_lines=$(wc -l < "$input_file")
+
+# Calculate the number of sublists needed
+num_sublists=$((total_lines / N))
+if [ $((total_lines % N)) -ne 0 ]; then
+    ((num_sublists++))
+fi
+
+if [ ! -d "$PWD/input/pPb8160/${formatted_date}" ]; then
+    echo "Directory '$PWD/input/pPb8160/${formatted_date}' does not exist. Creating..."
+    mkdir -p "$PWD/input/pPb8160/${formatted_date}"
+    if [ $? -eq 0 ]; then
+        echo "Directory created successfully."
+    else
+        echo "Failed to create directory. Terminating"
+        exit 1
+    fi
+fi
+
+# Create sublists
+for ((i = 0; i < num_sublists; i++)); do
+    start=$((i * N + 1))  # Calculate start line number for current sublist
+    end=$((start + N - 1))  # Calculate end line number for current sublist
+    sublist_file="${sample_prefix}_$((i+1)).list"  # Name of sublist file
+    # Extract sublist
+    sed -n "${start},${end}p" "$input_file" > "$PWD/input/pPb8160/${formatted_date}/$sublist_file"  
+done
+
+echo $num_sublists
+
---
+++ processing/submit_dijetAna.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Set path to CMSSW
+source $HOME/setup_cmsenv.sh
+
+# Initial parameters to run
+EXEC_PATH=${HOME}/soft/jetAnalysis/processing
+cd $EXEC_PATH
+
+# Date of submission
+formatted_date=$(date +"%Y%m%d")
+
+# Read input parameters
+# First parameters tells the dataset name: DATA_MB, DATA_HM185, DATA_HM250
+sample_name=DATA_MB
+
+# Beam direction
+is_Pbgoing=1
+direction=Pbgoing
+if [ "$is_Pbgoing" -eq 1 ]; then
+    ${direction}=Pbgoing
+else
+    ${direction}=pgoing
+fi
+
+# Dataset number
+pd_number=1
+
+# Final inputfile list
+input_file_list=unknown
+
+# Generate path to the inputfile list
+sample_prefix="inputfile"
+if [ "$sample_name" == "DATA_MB" ]; then
+    ${sample_prefix}=MB_PD${pd_number}_${direction}
+    ${input_file_list}=${EXEC_PATH}/filelists/pPb8160/DATA_MB/${direction}/${sample_prefix}.txt
+    
+elif [ "$sample_name" == "DATA_HM185" ]; then
+    ${sample_prefix}=HM185_PD${pd_number}_${direction}
+    ${input_file_list}=${EXEC_PATH}/filelists/pPb8160/DATA_HM185/${direction}/${sample_prefix}.txt
+    
+else
+    ${sample_prefix}=HM250_${direction}
+    ${input_file_list}=${EXEC_PATH}/filelists/pPb8160/DATA_HM250/${direction}/${sample_prefix}.txt
+fi
+
+# Specify number of files per list to split
+files_per_job=20
+#input_file_list=$HOME/filelists/test_list.txt
+
+echo -e "Splitting input file list: ${input_file_list}"
+n_sublists=$(./split_dijet_input.sh ${input_file_list} ${files_per_job} ${sample_name} ${direction} ${pd_number})
+echo -e "Input file list is splitted into ${n_sublists}"
+
+if [ ! -d "condor/sub/pPb8160/${formatted_date}" ]; then
+    echo "Directory 'condor/sub/pPb8160/${formatted_date}' does not exist. Creating..."
+    mkdir -p "condor/sub/pPb8160/${formatted_date}"
+    if [ $? -eq 0 ]; then
+        echo "Directory created successfully."
+    else
+        echo "Failed to create directory. Terminating"
+        exit 1
+    fi
+fi
+
+if [ ! -d "condor/log/pPb8160/${formatted_date}" ]; then
+    echo "Directory 'condor/log/pPb8160/${formatted_date}' does not exist. Creating..."
+    mkdir -p "condor/log/pPb8160/${formatted_date}"
+    if [ $? -eq 0 ]; then
+        echo "Directory created successfully."
+    else
+        echo "Failed to create directory. Terminating"
+        exit 1
+    fi
+fi
+
+cat <<EOF > condor/sub/pPb8160/${formatted_date}/pPb8160_${sample_prefix}.sub
+universe = vanilla
+executable = ${EXEC_PATH}/run_dijetAna.sh
++JobFlavour           = "longlunch"
+getenv     = True
+requirements =((OpSysAndVer =?= "AlmaLinux9") && (CERNEnvironment =?= "qa"))
+RequestCpus = 1
+transfer_input_files  = voms_proxy.txt
+environment = "X509_USER_PROXY=voms_proxy.txt"
+EOF
+
+
+for ((jobId = 1; jobId <= $n_sublists; jobId++)); do
+    cat <<EOF >>condor/sub/pPb8160/${formatted_date}/pPb8160_${sample_prefix}.sub
+arguments             = input/pPb8160/${formatted_date}/${prefix_sample}_$jobId.list ${sample_prefix}_pPb8160_$jobId.root 0 ${is_Pbgoing} 0 15000
+output                = condor/log/pPb8160/${formatted_date}/${prefix_sample}_$jobId.out
+error                 = condor/log/pPb8160/${formatted_date}/${prefix_sample}_$jobId.err
+log                   = condor/log/pPb8160/${formatted_date}/${prefix_sample}_$jobId.log
+queue 
+
+EOF
+
+done
+
+condor_submit condor/sub/pPb8160/${formatted_date}/pPb8160_${sample_prefix}.sub
+
+
