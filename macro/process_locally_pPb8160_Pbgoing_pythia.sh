c067791fa4abbed1616827e76b97fb57cf164733
Grigory Nigmatkulov <nigmatkulov@gmail.com>
2025-08-11 23:17:45 -0500

    Schedulers for MC have been updated

--- macro/process_locally_pPb8160_Pbgoing_pythia.sh
+++ macro/process_locally_pPb8160_Pbgoing_pythia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat15_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_15.root 2 1 15 30 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat30_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_30.root 2 1 30 50 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat50_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_50.root 2 1 50 80 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat80_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_80.root 2 1 80 120 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat120_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_120.root 2 1 120 170 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat170_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_170.root 2 1 170 220 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat220_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_220.root 2 1 220 280 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat280_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_280.root 2 1 280 370 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat370_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_370.root 2 1 370 460 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat460_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_460.root 2 1 460 540 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat540_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_540.root 2 1 540 8160 0 -99 0 1
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat15_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_15.root 2 1 15 30 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat30_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_30.root 2 1 30 50 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat50_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_50.root 2 1 50 80 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat80_Pbgoing_unembedded.root  oPythia_pPb8160_Pbgoing_80.root 2 1 80 120 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat120_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_120.root 2 1 120 170 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat170_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_170.root 2 1 170 220 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat220_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_220.root 2 1 220 280 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat280_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_280.root 2 1 280 370 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat370_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_370.root 2 1 370 460 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat460_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_460.root 2 1 460 540 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/Pbgoing/forest/HiForestSkim_pPb_MC_pthat540_Pbgoing_unembedded.root oPythia_pPb8160_Pbgoing_540.root 2 1 540 8160 0 -99 0 2
--- macro/process_locally_pPb8160_pgoing_pythia.sh
+++ macro/process_locally_pPb8160_pgoing_pythia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat15_pgoing_unembedded.root  oPythia_pPb8160_pgoing_15.root 2 0 15 30 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat30_pgoing_unembedded.root  oPythia_pPb8160_pgoing_30.root 2 0 30 50 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat50_pgoing_unembedded.root  oPythia_pPb8160_pgoing_50.root 2 0 50 80 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat80_pgoing_unembedded.root  oPythia_pPb8160_pgoing_80.root 2 0 80 120 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat120_pgoing_unembedded.root oPythia_pPb8160_pgoing_120.root 2 0 120 170 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat170_pgoing_unembedded.root oPythia_pPb8160_pgoing_170.root 2 0 170 220 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat220_pgoing_unembedded.root oPythia_pPb8160_pgoing_220.root 2 0 220 280 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat280_pgoing_unembedded.root oPythia_pPb8160_pgoing_280.root 2 0 280 370 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat370_pgoing_unembedded.root oPythia_pPb8160_pgoing_370.root 2 0 370 460 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat460_pgoing_unembedded.root oPythia_pPb8160_pgoing_460.root 2 0 460 540 0 -99 0 1
-../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat540_pgoing_unembedded.root oPythia_pPb8160_pgoing_540.root 2 0 540 8160 0 -99 0 1
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat15_pgoing_unembedded.root  oPythia_pPb8160_pgoing_15.root 2 0 15 30 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat30_pgoing_unembedded.root  oPythia_pPb8160_pgoing_30.root 2 0 30 50 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat50_pgoing_unembedded.root  oPythia_pPb8160_pgoing_50.root 2 0 50 80 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat80_pgoing_unembedded.root  oPythia_pPb8160_pgoing_80.root 2 0 80 120 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat120_pgoing_unembedded.root oPythia_pPb8160_pgoing_120.root 2 0 120 170 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat170_pgoing_unembedded.root oPythia_pPb8160_pgoing_170.root 2 0 170 220 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat220_pgoing_unembedded.root oPythia_pPb8160_pgoing_220.root 2 0 220 280 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat280_pgoing_unembedded.root oPythia_pPb8160_pgoing_280.root 2 0 280 370 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat370_pgoing_unembedded.root oPythia_pPb8160_pgoing_370.root 2 0 370 460 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat460_pgoing_unembedded.root oPythia_pPb8160_pgoing_460.root 2 0 460 540 0 -99 0 2
+../build/dijetAna_pPb8160 /Users/gnigmat/cernbox/ana/pPb8160/pythia/pgoing/forest/HiForestSkim_pPb_MC_pthat540_pgoing_unembedded.root oPythia_pPb8160_pgoing_540.root 2 0 540 8160 0 -99 0 2
