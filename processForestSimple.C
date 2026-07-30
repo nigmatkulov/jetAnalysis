197b2e51be2c1a45e20e718313c46f7aef3f5ea1
Grigory Nigmatkulov <nigmatkulov@gmail.com>
2026-07-29 23:44:48 -0500

    Accept HiForest files without AOD names

--- processForestSimple.C
+++ processForestSimple.C
@@ -1809,8 +1809,8 @@ void setupChains(const TString &input, TChain &hltTree, TChain &eventTree, TChai
                 //cout << "DEBUG found [" <<  file << "]" << endl;
 
                 // Check that file is of a correct name
-                if ( file.find(".root") != std::string::npos && file.find("Forest") != std::string::npos &&
-                     file.find("AOD") != std::string::npos ) {
+                if ( file.find(".root") != std::string::npos &&
+                     file.find("Forest") != std::string::npos ) {
                     
                     // Open file
                     TFile* ftmp = TFile::Open(file.c_str());
